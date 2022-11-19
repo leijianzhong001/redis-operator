106a32c604eb8ab89cba407e06d022cd0606563a
雷建忠 <18761665514@163.com>
2022-11-19 21:49:35 +0800

    提交

--- redis-Dockerfile-6.2.7/healthcheck.sh
+++ redis-Dockerfile-6.2.7/healthcheck.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 check_redis_health() {
-    if [[ -z "${REDIS_PASSWORD}" ]]; then
-        redis-cli ping
-    else
-        redis-cli -a ${REDIS_PASSWORD} ping
-    fi
+  if [[ -z "${REDIS_PASSWORD}" ]]; then
+    redis-cli ping
+  else
+    # redis-cli --user default --pass 123 ping
+    redis-cli --user "default" --pass "${REDIS_PASSWORD}" ping
+  fi
 }
 
 check_redis_health
